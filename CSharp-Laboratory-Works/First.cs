e1314820830d95665c07266841397c1003f42ab0
litolax <ura_rat@mail.ru>
2022-09-10 10:36:05 +0300

    Add first laboratory

---
+++ CSharp-Laboratory-Works/First.cs
@@ -0,0 +1,66 @@
+﻿namespace CSharp_Laboratory_Works;
+
+public interface IFirst
+{
+    void FirstTask();
+}
+
+public class First : IFirst
+{
+    public First()
+    {
+    }
+
+    public void FirstTask()
+    {
+        double y = 0, a = 0, x = 0, b = 0, z = 0, fi = 0;
+        Console.WriteLine("Введите a:");
+        a = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Введите b:");
+        b = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Введите z:");
+        z = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Введите число от 1 до 3 для выбора 1 из функций (2x, x^2, x/3)");
+        var variant = int.Parse(Console.ReadLine()!);
+
+        switch (variant)
+        {
+            case 1:
+            {
+                fi = 2 * x;
+                break;
+            }
+            case 2:
+            {
+                fi = Math.Pow(x, 2);
+                break;
+            }
+            case 3:
+            {
+                fi = x / 3;
+                break;
+            }
+        }
+
+        //Выбор x
+        switch (z)
+        {
+            case < 0:
+            {
+                x = z;
+                break;
+            }
+            case >= 0:
+            {
+                x = Math.Sin(z);
+                break;
+            }
+        }
+
+        //Выбор функции
+
+        y = 2 / 3 * a * Math.Pow(Math.Sin(x), 2) - 3 * b / 4 * Math.Pow(Math.Cos(fi), 2);
+        Console.WriteLine($"y = {y}");
+    }
+}
--- CSharp-Laboratory-Works/Program.cs
+++ CSharp-Laboratory-Works/Program.cs
@@ -1,7 +1,15 @@
-﻿class Program
+﻿using CSharp_Laboratory_Works;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
 {
     static void Main()
     {
-        
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IFirst, First>()
+            .BuildServiceProvider(true);
+
+        var first = serviceProvider.GetService<IFirst>();
+        first!.FirstTask();
     }
 }
