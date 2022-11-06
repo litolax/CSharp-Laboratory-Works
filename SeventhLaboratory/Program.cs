a3d3cc0611fb5706795d5c3239ee473b959ff6de
litolax <ura_rat@mail.ru>
2022-11-06 18:40:58 +0300

    Добавил лабы

---
+++ SeventhLaboratory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace SeventhLaboratory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First();
+            // Second();
+            // Third();
+            Fourth();
+            Console.ReadKey();
+        }
+
+        static void First()
+        {
+            var text = "Аврора будет ходить по Атомным станциям и находить там мало доллар";
+            var words = text.Split(' ').ToList();
+            var countsWordsWithA = words.Count(w => w.StartsWith("А"));
+            Console.WriteLine($"Количество слов начинающихся с А: {countsWordsWithA}");
+            var countsWordsWithLittle = words.Count(w => w.ToLowerInvariant() == "мало");
+            Console.WriteLine($"Количество слов мало: {countsWordsWithLittle}");
+            var newText = text.Replace("доллар", "рубль");
+            Console.WriteLine($"Новый текст: {newText}");
+        }
+
+        static void Second()
+        {
+            var textBefore = "Это предложение. без больших букв. и оно для тестирование. программы сделано";
+            var words = textBefore.Split(' ').ToList();
+            var countsWordsWithA = words.Count(w => w.StartsWith("А"));
+
+            var arr = textBefore.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i + 1 < arr.Length && arr[i] == '.')
+                {
+                    arr[i + 2] = arr[i + 2].ToString().ToUpperInvariant().ToCharArray()[0];
+                }
+            }
+
+            var textAfter = new string(arr);
+            Console.WriteLine($"Текст после: {textAfter}");
+        }
+
+        static void Third()
+        {
+            var textBefore = "Я Юра и я делаю лабораторную, помогите";
+            var arr = textBefore.ToCharArray().ToList();
+            arr.RemoveRange(0, arr.Count - 7);
+            arr.Sort();
+        }
+
+        static void Fourth()
+        {
+            var textBefore = "a aabbb gg";
+            var uniqueSymbols = string.Join("", textBefore.Distinct());
+            foreach (var element in uniqueSymbols)
+            {
+                Console.WriteLine($"Символ: {element}, встречается {textBefore.Count(e => e.Equals(element))}");
+            }
+
+            var spaceCounts = textBefore.Count(e => e.Equals(' '));
+            Console.WriteLine($"Количество пробелов в строке: {spaceCounts}");
+        }
+    }
+}
---
+++ SixthLaboratory/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SixthLaboratory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new List<int>
+            {
+                -1, 2, 8, 4, 0, -4
+            };
+
+            int sum = 0;
+            var first = array.IndexOf(array.First(f => f > 0));
+            var last = array.LastIndexOf(array.Last(f => f > 0));
+
+            for (int i = first; i <= last; i++) sum += array[i];
+
+            //Console.WriteLine($"Сумма = {sum}");
+
+            var array2 = new int[][]
+            {
+                new[] { 1, 2, -4 },
+                new[] { 1, 2, 3 },
+                new[] { 1, 0, 3 }
+            };
+            sum = 0;
+
+            var array3 = new int[][]
+            {
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 }
+            };
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                for (int j = 0; j < array2[i].Length; j++)
+                {
+                    array3[j][i] = array2[i][j];
+                }
+            }
+
+            for (int i = 0; i < array3.Length; i++)
+            {
+                if (array3[i].Any(a => a < 0)) continue;
+                for (int j = 0; j < array3[i].Length; j++)
+                {
+                    Console.WriteLine($"Сумма элементов столбца {j + 1} = {array3[i].Sum()}");
+                    break;
+                }
+            }
+
+            var random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                var array4 = new List<int>();
+                if (i != 2)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        array4.Add(random.Next(-10, 2));
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        array4.Add(random.Next(-10, 2));
+                    }
+                }
+
+                sum = Search(array4.ToArray());
+                Console.WriteLine($"Сумма = {sum}");
+                
+                
+            }
+        }
+
+        public static int Search(int[] array) => array.Where(a => a < 0).ToList().Sum();
+    }
+}
