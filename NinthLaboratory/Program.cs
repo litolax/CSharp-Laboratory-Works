e65f06d4eb5577c652bc8efeb2d7e87137713638
Yuri Ratkevich <ura_rat@mail.ru>
2022-11-07 23:52:24 +0300

    Добавил лабы

---
+++ NinthLaboratory/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace NinthLaboratory
+{
+    class Program
+    {
+        public struct Student
+        {
+            public string Name, Surname, SecondSurname;
+            public int YearOfBirthday;
+            public int Group;
+            public int PhysicsMark, MathMark, InformationMark, HemistryMark;
+            public int[] SortedMarks;
+
+            public void InputData()
+            {
+                Console.WriteLine("Введите имя: ");
+                this.Name = Console.ReadLine();
+                Console.WriteLine("Введите фамилию: ");
+                this.Surname = Console.ReadLine();
+                Console.WriteLine("Введите отчество: ");
+                this.SecondSurname = Console.ReadLine();
+                Console.WriteLine("Введите год рождения: ");
+                this.YearOfBirthday = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите номер группы: ");
+                this.Group = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите отметку за семестр по химии: ");
+                this.HemistryMark = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите отметку за семестр по физике: ");
+                this.PhysicsMark = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите отметку за семестр по математике: ");
+                this.MathMark = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите отметку за семестр по информатике: ");
+                this.InformationMark = Convert.ToInt32(Console.ReadLine());
+            }
+
+            public string GetFIO()
+            {
+                return $"{Surname} {Name[0]}.{SecondSurname[0]}";
+            }
+
+            public double GetAverageMark()
+            {
+                return (MathMark + HemistryMark + PhysicsMark + InformationMark) / 4;
+            }
+
+            enum Marks
+            {
+                Math,
+                Hemistry,
+                Physics,
+                Information
+            }
+
+            public int[] Sort()
+            {
+                this.SortedMarks = new [] { MathMark, PhysicsMark, InformationMark, HemistryMark };
+                int temp;
+                for (var i = 0; i < 3; i++)
+                {
+                    var min = i;
+                    for (var j = i + 1; j < 4; j++)
+                    {
+                        if (SortedMarks[j] < SortedMarks[min])
+                            min = j;
+                    }
+
+                    temp = SortedMarks[i];
+                    SortedMarks[i] = SortedMarks[min];
+                    SortedMarks[min] = temp;
+                }
+
+                return SortedMarks;
+            }
+
+            public void ShowMarks() =>
+                Console.WriteLine(
+                    $"Отметка {Name} по математике {MathMark}, по физике {PhysicsMark}, по информатике {InformationMark}, по химии {HemistryMark}");
+
+            public void ShowSortedMarks()
+            {
+                Console.WriteLine($"Отсортированные отметки {Name}:{SortedMarks[0]}, {SortedMarks[1]}, {SortedMarks[2]}, {SortedMarks[3]}");
+            }
+
+            public void ShowInfo() =>
+                Console.WriteLine(
+                    $"Имя: {GetFIO()}.\nГод рождения: {YearOfBirthday}.\nНомер группы: {Group}\nСредний балл: {GetAverageMark()}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько студентов вы хотите ввести? ");
+            var studentsCount = int.Parse(Console.ReadLine());
+            var students = new Student[studentsCount];
+            for (var i = 0; i < studentsCount; i++)
+            {
+                students[i] = new Student();
+                students[i].InputData();
+            }
+
+            Console.WriteLine("Введите интересующую вас группу: ");
+            var groupIndex = int.Parse(Console.ReadLine());
+
+            var neededStudents = students.ToList()
+                .Where(s => s.Group == groupIndex && s.PhysicsMark == 9 && s.MathMark == 10);
+
+            Console.WriteLine($"Ученики из группы {groupIndex} с отметками 9 по физике и 10 по математике: ");
+            foreach (var student in neededStudents)
+            {
+                Console.WriteLine(student.GetFIO());
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TenthLaboratory/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EigthLaboratory
+{
+    class Test
+    {
+        public int FirstInt;
+        public int SecondInt;
+        public int ThirdInt;
+        public string FirstString;
+        public string SecondString;
+        public string ThirdString;
+
+        public virtual void Testing()
+        {
+            FirstInt = 1;
+            SecondInt = 2;
+            ThirdInt = 3;
+            FirstString = "1";
+            SecondString = "2";
+            ThirdString = "3";
+               
+        }
+    }
+
+    class Test2 : Test
+    {
+        public int FourthInt;
+        public string FourthString;
+
+        public override void Testing()
+        {
+            base.Testing();
+            FourthInt = 4;
+            FirstInt = 5;
+            
+            Console.WriteLine(FirstInt);
+        }
+    }
+
+    class AVTO
+    {
+        public int YearOfGraduate;
+
+        public AVTO()
+        {
+            Console.WriteLine("Вызван конструктор без параметров");
+        }
+        
+        public AVTO(int year)
+        {
+            YearOfGraduate = year;
+            Console.WriteLine("Вызван конструктор с параметрами");
+        }
+    }
+    
+    class AVTO1 : AVTO
+    {
+        public AVTO1()
+        {
+            Console.WriteLine("Вызван конструктор без параметров в классе наследнике");
+        }
+       
+        public AVTO1(int year) : base(year)
+        {
+            Console.WriteLine("Вызван конструктор с параметрами в классе наследнике");
+        }
+        public void InputData()
+        {
+            Console.WriteLine("Введите год выпуска автомобиля");
+            YearOfGraduate = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        public void ShowData()
+        {
+            Console.WriteLine("Год выпуска автомобиля: " + YearOfGraduate);
+        }
+    }
+
+    class Time
+    {
+        public int Seconds;
+        public int Minutes;
+        public int Hours;
+    }
+
+    class Time2 : Time
+    {
+        public void SetSeconds(int seconds)
+        {
+            if (seconds >= 0 && seconds <= 59)
+            {
+                Seconds = seconds;
+            }
+            else throw new Exception("Некорректное значение секунд");
+        }
+        
+        public void SetMinutes(int minutes)
+        {
+            if (minutes >= 0 && minutes <= 59)
+            {
+                Minutes = minutes;
+            }
+            else throw new Exception("Некорректное значение минут");
+        }
+        
+        public void SetHours(int hours)
+        {
+            if (hours >= 0 && hours <= 59)
+            {
+                Hours = hours;
+            }
+            else throw new Exception("Некорректное значение часов");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test2 test2 = new Test2();
+            
+            test2.Testing();
+            
+            AVTO1 avto1 = new AVTO1();
+            
+            avto1.InputData();
+            avto1.ShowData();
+
+            AVTO1 avto2 = new AVTO1(2004);
+            avto2.ShowData();
+            
+            Time2 time2 = new Time2();
+            time2.SetHours(10);
+            time2.SetMinutes(25);
+            time2.SetSeconds(30);
+
+            Console.ReadKey();
+        }
+    }
+}
